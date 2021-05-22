ae03014e6bb7ed800ce6f832c6d0af12a2b1941c
Tien Tai <Tientaiphan.joe@gmail.com>
2021-05-22 09:43:32 +0700

    Add 10 fold cross validation module

---
+++ weka-api/src/weka/api/AssociationManager.java
@@ -0,0 +1,103 @@
+package weka.api;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+
+import weka.associations.AssociationRule;
+import weka.associations.FPGrowth;
+import weka.associations.Item;
+import weka.associations.NominalItem;
+import weka.core.Instance;
+import weka.core.Instances;
+
+public class AssociationManager {
+	
+	private FPGrowth fpGrowth;
+	private boolean log = false;
+	private String defaultValue = "n";
+	
+	public AssociationManager(Instances input) throws Exception {
+		fpGrowth = new FPGrowth();
+		fpGrowth.buildAssociations(input);
+	}
+	
+	public void setLog(boolean log) {
+		this.log = log;
+	}
+	
+	public void setDefault(String defaultValue) {
+		this.defaultValue = defaultValue;
+	}
+	
+	public boolean test(Instance target) throws Exception {
+		// create random generator
+		Random rand = new Random();
+		int randomIdx = rand.nextInt(target.numAttributes());
+		
+		// pick a value and remove it from the target
+		String valueAtMissing = target.stringValue(randomIdx);
+		target.setMissing(randomIdx);
+		
+		String prediction = defaultValue;
+		
+		boolean found = false;
+		for(AssociationRule r : fpGrowth.getAssociationRules().getRules()) {
+			for(Item item : r.getConsequence()) {
+				// Check if r appears in the right side of the rule
+				if(item.getAttribute().index() != randomIdx)
+					continue;
+				
+				// Check if the premises are met
+				boolean allMet = true;
+				for(Item premI : r.getPremise()) {
+					NominalItem prem = (NominalItem) premI;
+					int premIdx = prem.getAttribute().index();
+					String src = target.stringValue(premIdx);
+					String cur = prem.getItemValueAsString();
+					if (src.compareTo(cur) != 0) {
+						allMet = false;
+						break;
+					}
+				}
+				
+				if(allMet) {
+					found = true;
+					prediction = item.getItemValueAsString();
+					break;
+				}
+			}
+			
+			if(found)
+				break;
+		}
+		
+		boolean result = prediction.equals(valueAtMissing);
+	
+		if(log) {
+			System.out.println(target);
+			System.out.println(randomIdx + " " + valueAtMissing);
+			System.out.println("predicted: " + prediction);
+			System.out.println("correct: " + result);
+			System.out.println();
+		}
+		
+		return result;
+	}
+	
+	public void printRules() {
+		List<AssociationRule> rules = fpGrowth.getAssociationRules().getRules();
+		for(AssociationRule r : rules) {
+			System.out.println();
+			System.out.println("For rule " + r);
+			System.out.println("Premises: ");
+			Collection<Item> prem = r.getPremise();
+			for(Item item: prem)
+				System.out.println("\t" + item);
+			System.out.println("Consequences: ");
+			Collection<Item> cons = r.getConsequence();
+			for(Item item : cons)
+				System.out.println("\t" + item);
+		}
+	}
+}
---
+++ weka-api/src/weka/api/DBScanClustering.java
@@ -0,0 +1,58 @@
+package weka.api;
+
+import java.util.HashMap;
+import weka.clusterers.DBSCAN;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.EuclideanDistance;
+
+public class DBScanClustering {
+	private Instances instances;
+	private int numberOfCluster;
+	private double epsilon = 0.9;
+	private int minPoints = 5;
+	protected DBSCAN dbscan = new DBSCAN();
+	
+	public DBScanClustering(Instances instances, double epsilon, int minPoints) {
+		this.instances = instances;
+		this.epsilon = epsilon;
+		this.minPoints = minPoints;
+	}
+	
+	public void processing() throws Exception {
+		this.dbscan.setEpsilon(this.epsilon);
+		this.dbscan.setMinPoints(this.minPoints);
+		this.dbscan.setDistanceFunction(new EuclideanDistance());
+		this.dbscan.buildClusterer(this.instances);
+		this.numberOfCluster = dbscan.numberOfClusters();
+	}
+	
+	public int getNumberOfCluster() throws Exception {
+		return this.numberOfCluster;
+	}
+	
+	public int getCluster(Instance instance) throws Exception {
+		return this.dbscan.clusterInstance(instance);
+	}
+	
+	public HashMap<Integer, Instances> getClusteredInstances() throws Exception {
+		HashMap<Integer, Instances> clusteredInstances = new HashMap<Integer, Instances>();
+		for (int i = -1; i < this.numberOfCluster; i++) {
+			clusteredInstances.put(new Integer(i), new Instances(this.instances, 0, 0));
+		}
+		for (int i = 0; i < this.instances.numInstances(); i++) {
+			Instance instance = this.instances.get(i);
+			int cluster;
+			try {
+				cluster = dbscan.clusterInstance(instance);
+			} catch(Exception e) {// If the instance is considered as a noise, dbscan will throw an exception
+				cluster = -1;
+			}
+			Instances oldInstances = clusteredInstances.get(cluster);
+			oldInstances.add(instance);
+			clusteredInstances.replace(cluster, oldInstances);
+		}
+		return clusteredInstances;
+	}
+	
+}
